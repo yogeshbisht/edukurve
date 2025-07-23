1f9659040d89fad2f8994528bd1350c28726eb54
Yogesh Bisht <reinzire@gmail.com>
2025-07-23 14:08:52 +0530

    initialize shadcn components setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return <div className="font-semibold text-5xl">Hello World</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen space-y-2">
+      <h1 className="font-bold text-5xl">Hello World</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default HomePage;
